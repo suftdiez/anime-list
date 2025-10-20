26da9885ad3316bce171cc034129e76c63fbf7e6
yusuf <fmygames08@gmail.com>
2025-10-21 00:18:46 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,9 @@ const Home = async () => {
 
   return (
     <div>
-      <div className="flex justify-between p-4">
+      <div className="flex justify-between items-center p-4">
       <h1 className="text-2xl font-bold">Paling Populer</h1>
-      <Link href="/popular" className="text-xl underline hover:text-indigo-500 transition-all">Lihat Semua</Link>
+      <Link href="/popular" className="md:text-xl text-md underline hover:text-indigo-500 transition-all">Lihat Semua</Link>
       </div>
       <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
         {anime.data.map((data) => {
