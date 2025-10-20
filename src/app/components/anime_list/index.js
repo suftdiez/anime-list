1ff25cc110f9d24047aabc487636074772666d48
yusuf <fmygames08@gmail.com>
2025-10-20 23:58:49 +0700

    update

--- src/app/components/anime_list/index.js
+++ src/app/components/anime_list/index.js
@@ -2,11 +2,16 @@ import Link from "next/link"
 
 const AnimeList = ({title, images, id}) => {
   return (
-      <Link href={`/${id}`} className="pointer"> 
-        <img
-          src={images} alt={title} width="350" height="350"/>
-          <h3 className="font-bold md:text-xl p-4 text-md">{title}</h3>
-        </Link>
+      <Link href={`/${id}`} className="pointer block overflow-hidden rounded-lg"> 
+        <div className="overflow-hidden bg-gray-200">
+          <img
+            src={images} 
+            alt={title} 
+            className="w-full h-auto"
+          />
+        </div>
+        <h3 className="font-bold md:text-xl p-4 text-md line-clamp-2">{title}</h3>
+      </Link>
   );
 };
 
