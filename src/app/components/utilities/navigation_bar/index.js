ba5d42c9b2c7648f3fcb978518a1b7cb757a8b21
yusuf <fmygames08@gmail.com>
2025-10-20 23:51:37 +0700

    new update

--- src/app/components/utilities/navigation_bar/index.js
+++ src/app/components/utilities/navigation_bar/index.js
@@ -3,17 +3,17 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <header className="bg-indigo-500 shadow-lg w-full">
-      <div className="flex flex-col sm:flex-row sm:justify-between items-center p-4 max-w-7xl mx-auto gap-4 sm:gap-0 px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col sm:flex-row justify-between items-center p-4 max-w-7xl mx-auto gap-4 sm:gap-0 px-4 sm:px-6 lg:px-8">
         <Link
           href="/"
           className="font-bold text-white text-xl hover:text-indigo-200 transition-colors"
         >
           ANIME LIST
         </Link>
-        <div className="relative w-full sm:w-auto sm:ml-auto">
+        <div className="relative w-full sm:w-auto">
           <input
             placeholder="Cari Anime..."
-            className="bg-white rounded-full px-6 py-2 w-full sm:w-[300px] focus:outline-none focus:ring-2 focus:ring-indigo-300 transition-all shadow-sm"
+            className="bg-white rounded-full px-6 py-3 my-2 w-full sm:w-[300px] focus:outline-none focus:ring-2 focus:ring-indigo-300 transition-all shadow-sm"
           />
         </div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
 import AnimeList from "@/app/components/anime_list";
+import Link from "next/link";
 const Home = async () => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
   );
   const anime = await response.json();
 
   return (
     <div>
-      <h1>Paling Populer</h1>
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+      <div className="flex justify-between p-4">
+      <h1 className="text-2xl font-bold">Paling Populer</h1>
+      <Link href="/popular" className="text-xl underline hover:text-indigo-500 transition-all">Lihat Semua</Link>
+      </div>
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
         {anime.data.map((data) => {
           return (
             <div key={data.mal_id} className="shadow-xl">
